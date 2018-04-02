33fc4d4a83784e85f5743cea999e4d6782ca7cfc
adamcee <adam.cahan@gmail.com>
2018-04-02 16:40:24 -0500

    - begin refactoring structs for better creation of structs from json response body. in progress, but should be nonbreaking. 	modified:   lib/reverb_server/types.ex

--- reverb_server/lib/reverb_server/types.ex
+++ reverb_server/lib/reverb_server/types.ex
@@ -4,29 +4,14 @@ defmodule ReverbServer.Types do
   """
 
   alias ReverbServer.Types, as: T
+  alias ReverbServer.Utils, as: U
 
-  defmodule Href do
-    @enforce_keys [:href]
-    defstruct [:href]
-    @type t :: %Href{href: String.t()}
-  end
-
-  defmodule CategoryLinks do
-    @enforce_keys [:collection_header_image, :follow, :image,
-      :listings, :self, :follow, :web]
-    defstruct [:collection_header_image, :follow, :image,
-      :listings, :self, :follow, :web]
-
-    @type t:: %CategoryLinks {
-      collection_header_image: %Href{},
-      follow: %Href{},
-      image: %Href{},
-      listings: %Href{},
-      self: %Href{},
-      follow: %Href{},
-      web: %Href{}
-    }
-
+  defmodule CategoriesFlatResponseBody do
+    defstruct [:categories]
+    @enforce_keys [:categories]
+    @type t :: %CategoriesFlatResponseBody {
+                 categories: list(T.Category)
+               }
   end
 
   defmodule Category do
@@ -38,16 +23,45 @@ defmodule ReverbServer.Types do
       :slug, :uuid]
 
     @type t :: %Category{
-      _links: %CategoryLinks{},
-      collection_title: String.t(),
-      full_name: String.t(),
-      listable: boolean(),
-      name: String.t(),
-      root_slug: String.t(),
-      root_uuid: String.t(),
-      slug: String.t(),
-      uuid: String.t(),
+                 _links: T.CategoryLinks,
+                 collection_title: String.t(),
+                 full_name: String.t(),
+                 listable: boolean(),
+                 name: String.t(),
+                 root_slug: String.t(),
+                 root_uuid: String.t(),
+                 slug: String.t(),
+                 uuid: String.t(),
+               }
+
+    # def init(map_json) do
+    #   cat_links = CategoryLinks.init(map_json["_links"])
+    # end
+  end
+
+  defmodule CategoryLinks do
+    @enforce_keys [:collection_header_image, :follow, :image,
+      :listings, :self, :follow, :web]
+    defstruct [:collection_header_image, :follow, :image,
+      :listings, :self, :follow, :web]
+
+    @type t:: %CategoryLinks {
+      collection_header_image: String.t,
+      follow: String.t,
+      image: String.t,
+      listings: String.t,
+      self: String.t,
+      follow: String.t,
+      web: String.t,
     }
+
+    # def init(map_json) do
+    #   # Convert like {"follow": "href" => "http://google.com"}
+    #   # to  {"follow": "http://google.com"}, etc
+    #   map_json
+    #   |> Enum.map(fn container => container["href"] end)
+    #   |> fn parsed -> struct(CategoryLinks, parsed) end
+    # end
   end
 
  end
