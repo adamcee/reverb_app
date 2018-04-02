41ddb696230f0fb8bdfe594df1183f766dd68d90
adamcee <adam.cahan@gmail.com>
2018-04-02 16:36:54 -0500

    - add utility function to convert a flat map with string keys into a given struct. - comment out unused validateUri utility function. Save for posterity. 	modified:   .idea/workspace.xml 	modified:   lib/reverb_server/http_client_helpers.ex 	modified:   lib/reverb_server/reverb_api.ex 	modified:   lib/reverb_server/types.ex

--- reverb_server/lib/reverb_server/utils.ex
+++ reverb_server/lib/reverb_server/utils.ex
@@ -3,17 +3,40 @@ defmodule ReverbServer.Utils do
   Utility/Helper functions
   """
 
-  # from: https://stackoverflow.com/posts/30700541/revisions
-  defmodule ValidateUri do
-    def validate_uri(str) do
-      uri = URI.parse(str)
-      case uri do
-        %URI{scheme: nil} -> {:error, uri}
-        %URI{host: nil} -> {:error, uri}
-        %URI{path: nil} -> {:error, uri}
-        uri -> {:ok, uri}
+#  # from: https://stackoverflow.com/posts/30700541/revisions
+#  defmodule ValidateUri do
+#    def validate_uri(str) do
+#      uri = URI.parse(str)
+#      case uri do
+#        %URI{scheme: nil} -> {:error, uri}
+#        %URI{host: nil} -> {:error, uri}
+#        %URI{path: nil} -> {:error, uri}
+#        uri -> {:ok, uri}
+#      end
+#    end
+#  end
+#
+#end
+#
+#
+ @doc """
+ Convert a map with string keys to a struct
+ from: https://groups.google.com/forum/#!msg/elixir-lang-talk/6geXOLUeIpI/L9einu4EEAAJ
+
+  kind: The struct to create
+  attr: A map with string keys
+
+  Returns a struct of type kind
+  NOTE: Be careful using this with structs with nested structures!!
+ """
+ def to_struct(kind, attrs) do
+    struct = struct(kind)
+    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
+      case Map.fetch(attrs, Atom.to_string(k)) do
+        {:ok, v} -> %{acc | k => v}
+        :error -> acc
       end
     end
-  end
+   end
 
 end
