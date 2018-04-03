72d3ff910b27436bde9ae99d0309294e726eaaa5
adamcee <adam.cahan@gmail.com>
2018-04-03 11:49:21 -0500

    - create Utils.get_file and get_json_file_throws_err - add tests 	modified:   lib/reverb_server/utils.ex 	modified:   test/reverb_server/utils_test.exs

--- reverb_server/lib/reverb_server/utils.ex
+++ reverb_server/lib/reverb_server/utils.ex
@@ -5,53 +5,76 @@ defmodule ReverbServer.Utils do
 
   require Logger
 
-#  # from: https://stackoverflow.com/posts/30700541/revisions
-#  defmodule ValidateUri do
-#    def validate_uri(str) do
-#      uri = URI.parse(str)
-#      case uri do
-#        %URI{scheme: nil} -> {:error, uri}
-#        %URI{host: nil} -> {:error, uri}
-#        %URI{path: nil} -> {:error, uri}
-#        uri -> {:ok, uri}
-#      end
-#    end
-#  end
-#
-#end
-#
-#
- @doc """
- Convert a map with string keys to a struct
- from: https://groups.google.com/forum/#!msg/elixir-lang-talk/6geXOLUeIpI/L9einu4EEAAJ
-
-  kind: The struct to create
-  attr: A map with string keys
-
-  Returns a struct of type kind
-  NOTE: Be careful using this with structs with nested structures!!
- """
- def to_struct(kind, attrs) do
-    struct = struct(kind)
-    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
-      case Map.fetch(attrs, Atom.to_string(k)) do
-        {:ok, v} -> %{acc | k => v}
-        :error -> acc
-      end
-    end
-   end
-
 
   @doc """
-  returns {:ok, json} | {:error, reason}
+  Returns `%{}`
+  Throws err if fails. Useful when getting json files for tests
   """
-  def get_json_file(path_from_proj_root) do
+  def get_json_file_throws_err(path_from_proj_root) do
     path = Path.join(File.cwd!, path_from_proj_root)
-    case File.read(path) do
-      {:ok, body} -> {:ok, Poison.decode!(body)}
-      {:error, reason} -> {:error, reason}
-       _ -> {:error, :get_json_file_failure}
+    case File.read!(path) do
+      body -> {:ok, Poison.decode!(body)}
+      _ -> {:error, :failure}
     end
   end
+
+  @doc """
+  Returns `{:ok, %{}}` | `{:error, reason}`
+  """
+  def get_json_file(path_from_proj_root) do
+    case get_file(path_from_proj_root) do
+      {:ok, body} -> {:ok, Poison.decode!(body)}
+      {:error, reason} -> {:error, reason}
+       _ -> {:error, :failure}
+    end
+  end
+
+  @doc """
+  Returns `{:ok, String.t}` | `{:error, reason}`
+  """
+  def get_file(path_from_proj_root) do
+    path = Path.join(File.cwd!, path_from_proj_root)
+    case File.read(path) do
+      {:ok, body} -> {:ok, body}
+      {:error, reason} -> {:error, reason}
+      _ -> {:error, :failure}
+    end
+  end
+
+  #  # from: https://stackoverflow.com/posts/30700541/revisions
+  #  defmodule ValidateUri do
+  #    def validate_uri(str) do
+  #      uri = URI.parse(str)
+  #      case uri do
+  #        %URI{scheme: nil} -> {:error, uri}
+  #        %URI{host: nil} -> {:error, uri}
+  #        %URI{path: nil} -> {:error, uri}
+  #        uri -> {:ok, uri}
+  #      end
+  #    end
+  #  end
+  #
+  #end
+  #
+  #
+  # @doc """
+  # Convert a map with string keys to a struct
+  # from: https://groups.google.com/forum/#!msg/elixir-lang-talk/6geXOLUeIpI/L9einu4EEAAJ
+
+  #  kind: The struct to create
+  #  attr: A map with string keys
+
+  #  Returns a struct of type kind
+  #  NOTE: Be careful using this with structs with nested structures!!
+  # """
+  # def to_struct(kind, attrs) do
+  #    struct = struct(kind)
+  #    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
+  #      case Map.fetch(attrs, Atom.to_string(k)) do
+  #        {:ok, v} -> %{acc | k => v}
+  #        :error -> acc
+  #      end
+  #    end
+  #   end
 
 end
--- reverb_server/test/reverb_server/utils_test.exs
+++ reverb_server/test/reverb_server/utils_test.exs
@@ -1,28 +1,51 @@
 defmodule ReverbServer.UtilsTest do
   ExUnit.start()
 
-  defmodule GetJsonFileTest do
+  defmodule GetFileTest do
     use ExUnit.Case, async: true
     alias ReverbServer.Utils, as: U
 
-    test "ReverbServer.Utils.get_json_file returns error for a nonexistent file" do
+    test "ReverbServer.Utils.get_file returns error for a nonexistent file" do
       case U.get_json_file("mock_data/this_definitely_does_not_exist.json") do
         {:error, _ } -> assert true
+        _ -> assert false
+      end
+    end
+
+    test "ReverbServer.Utils.get_file correctly retrieves a file." do
+      case U.get_file("mock_data/mock_categories.json") do
+        {:ok, body} when is_binary(body) -> assert true
          _ -> assert false
       end
     end
 
+  end
+
+  defmodule GetJsonFileTest do
+    use ExUnit.Case, async: true
+    alias ReverbServer.Utils, as: U
+
     test "ReverbServer.Utils.get_json_file correctly retrieves and parses a .json file." do
       case U.get_json_file("mock_data/mock_categories.json") do
-        {:error, _ } -> assert false
         {:ok, json} ->
           case Map.has_key?(json, "categories") do
             true -> assert true
             false -> assert false
           end
+          _ -> assert false
       end
     end
 
+    test "ReverbServer.Utils.get_json_file_throws_err correctly retrieves and parses a .json file." do
+      case U.get_json_file("mock_data/mock_categories.json") do
+        {:ok, json} ->
+          case Map.has_key?(json, "categories") do
+            true -> assert true
+            false -> assert false
+          end
+        _ -> assert false
+      end
+    end
   end
 
 end
