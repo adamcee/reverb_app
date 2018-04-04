b1b13e4cc0b4b5269712c88c9be5a3b1e72fee87
adamcee <adam.cahan@gmail.com>
2018-04-03 19:25:05 -0500

    - moved Link tsts into their own file. 	copied:     test/reverb_server/types_test.exs -> test/reverb_server/link_types_test.exs 	modified:   test/reverb_server/types_test.exs

--- reverb_server/test/reverb_server/types_test.exs
+++ reverb_server/test/reverb_server/types_test.exs
@@ -16,28 +16,4 @@ defmodule ReverbServer.TypesTest do
     end
   end
 
-  defmodule LinkTest do
-    use ExUnit.Case, async: true
-    test "assert Link.from_str_map creates a Link structure from json" do
-      # get map of json from string, like we would from an http response
-      {:ok, json} = Poison.encode(%{href: "http://google.com"})
-      json = Poison.decode!(json)
-
-      case T.Link.from_str_map(json) do
-        _correct = %T.Link{} -> assert true
-        _ -> assert false
-      end
-    end
-
-    test "assert Link.from_str_map converts method val to atom - \"get\" to :get" do
-      # get map of json from string, like we would from an http response
-      {:ok, json} = Poison.encode(%{href: "http://google.com", method: "get"})
-      json = Poison.decode!(json)
-
-      case T.Link.from_str_map(json) do
-       %T.Link{href: "http://google.com", method: :get} -> assert true
-      end
-    end
-  end
-
 end
