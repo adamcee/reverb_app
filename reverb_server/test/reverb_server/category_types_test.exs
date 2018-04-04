0c1dd25ebc70cdeb433e3d5078231c949a57b198
adamcee <adam.cahan@gmail.com>
2018-04-03 19:26:34 -0500

    - moved flatten_href test into CategoryTypesTest 	modified:   test/reverb_server/category_types_test.exs 	deleted:    test/reverb_server/types_test.exs

--- reverb_server/test/reverb_server/category_types_test.exs
+++ reverb_server/test/reverb_server/category_types_test.exs
@@ -6,6 +6,18 @@ defmodule ReverbServer.CategoryTypesTest do
   alias ReverbServer.Utils, as: U
   ExUnit.start()
 
+  defmodule HelpersTest do
+    use ExUnit.Case, async: true
+    test "assert flatten_href returns an atom map containing the link value" do
+      mock = {"google", %{"href" => "http://google.com"}}
+      case T.flatten_href(mock) do
+        {:google, "http://google.com"} -> assert true
+        _ -> assert false
+      end
+    end
+  end
+
+
   defmodule CategoryLinksTest do
     use ExUnit.Case, async: true
     test "assert CategoryLinks.from_str_map creates a CategoryLinks struct from json" do
